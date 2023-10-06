8511bdd388a2756dfbd83202e7068cd6eabd8139
Mochatr <mohamedchatr8@gmail.com>
2023-10-05 17:52:11 -0700

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,11 +7,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-  
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     op = sys.argv[2]
-
     """Arithmetic Operations"""
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
