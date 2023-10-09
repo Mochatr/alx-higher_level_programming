1f19c7206deff8763e55c190c04ed3d29ce2852d
Mochatr <mohamedchatr8@gmail.com>
2023-10-09 16:44:41 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) > 1:
-        return (len(sentence), sentence[0])
+    length = len(sentence)
+    if length > 0:
+        first_character = sentence[0]
     else:
-        return None
+        first_character = None
+    return (length, first_character)
