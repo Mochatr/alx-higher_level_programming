8f8b0ee883e413080da125e59a810bf686d8c76a
Mochatr <mohamedchatr8@gmail.com>
2023-10-09 13:55:06 -0700

    Update

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    #Modified string
+    result_str = ""
+    for idx in range(len(my_string)):
+        if (my_string[idx] != 'c' and my_string[idx] != 'c'):
+            result_str += my_string[idx]
+        return result_str
