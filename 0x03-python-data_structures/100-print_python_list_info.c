74808eff74aae0d6eef9e74c12c670dffa1df7c2
Mochatr <mohamedchatr8@gmail.com>
2023-10-09 18:27:02 -0700

    update

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - function
+ * @p: parameter
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int list_size, allocated_size, i;
+	pyObject *list_item;
+
+	list_size = Py_SIZE(p);
+	allocated_size = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n",list_size);
+	printf("[*] Allocated = %d\n", allocated_size);
+
+	for (i = 0; i < list_size; i++)
+	{
+		printf("Element %d: ", i);
+
+		list_item = PyList_GelItem(p, i);
+		printf("%s\n", Py_TYPE(list_item)->tp_name);
+	}
+}
