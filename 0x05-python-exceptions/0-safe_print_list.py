453200a3323cc59bcd50ccdc2ac599b3f8b0a02b
Mochatr <mohamedchatr8@gmail.com>
2023-10-23 12:46:37 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
     try:
         while idx is not x:
-            print(my_list[i], end="")
-            i += 1
+            print(my_list[idx], end=" ")
+            idx += 1
     except IndexError:
         None
     print()
