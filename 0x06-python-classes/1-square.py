1dff8ea147d2b5757ed8c95488a945c14455971b
Mochatr <mohamedchatr8@gmail.com>
2023-10-24 13:33:00 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+
+        """Parameter: the size of the square"""
+        self.size = size
