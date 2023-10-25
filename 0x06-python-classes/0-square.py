480141b964511e315520ba16592fc7b579b8f312
Mochatr <mohamedchatr8@gmail.com>
2023-10-25 02:58:48 -0700

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,4 +2,13 @@
 
 class Square:
     """This class defines a a square."""
-    pass
+    
+    def __init__(self, side_length):
+        """
+        Initializes a square with a given
+        side length.
+
+        Args:
+        side_length(int): the length of each side of the square.
+        """
+        self.side_length = side_length
