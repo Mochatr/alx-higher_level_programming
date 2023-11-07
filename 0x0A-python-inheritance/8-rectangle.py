3c1d5fd325faf375e6bd470c85e53022599cda9d
Mochatr <mohamedchatr8@gmail.com>
2023-11-06 17:30:39 -0800

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """ Module for the Rectangle class """
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+from 7-base_geometry import BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Thins class inherites from BaseGeometry """
+    """ Thins class inherites BaseGeometry """
     def __init__(self, width, height):
         """ Constructor """
+        super().__init()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """ Method """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """ Method """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
