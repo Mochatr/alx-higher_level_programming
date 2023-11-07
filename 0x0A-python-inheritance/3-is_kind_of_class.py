9e6380e914fee6a7138b5d4ba328b90cf5fce594
Mochatr <mohamedchatr8@gmail.com>
2023-11-06 16:34:41 -0800

    update

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Verifies whether an object is a subclass of a class
+    """
+    return isinstance(obj, a_class)
