fec5601283fea3b1bae351d56727d97c31c1e0f4
Mochatr <mohamedchatr8@gmail.com>
2023-11-06 16:40:32 -0800

    update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Method for the inherits_from method """
+
+
+def inherits_from(obj, a_class):
+    """
+    Verifies whether an object is a subclass of a class
+    """
+    return isinstance(obj, a_class) and obj.__class__ is a_class
