bfc1f2efeda200398765afa8e0cd10c588893c7b
Mochatr <mohamedchatr8@gmail.com>
2024-01-25 13:41:01 -0800

    update

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of unsorted integers """
+
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return None
