e104fdab1f19d65c19ca226bfd0ce5f399d7fa3a
Mochatr <mohamedchatr8@gmail.com>
2024-01-26 09:33:24 -0800

    update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Module """
+import urllib.request
+
+
+def fetch_status():
+    """ fetches https://alx-intranet.hbtn.io/status """
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    fetch_status()
