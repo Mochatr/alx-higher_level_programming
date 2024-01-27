254a1535e50875f153ad693e1da1ee4305509cbc
Mochatr <mohamedchatr8@gmail.com>
2024-01-27 11:02:25 -0800

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ sends a POST request to the passed URL with the email as a parameter
 and displays the body of the response (decoded in utf-8)
 """
 
-import urllib.request import urlopen
+from urllib.request import urlopen
 import sys
 
 
