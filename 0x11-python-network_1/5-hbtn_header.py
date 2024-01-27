4f7ed2382e13002b52dc4b942f8f2fe3ea174f6c
Mochatr <mohamedchatr8@gmail.com>
2024-01-27 11:26:25 -0800

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,20 +10,8 @@ import requests
 import sys
 
 
-def search_x_request_id(url):
-    """
-    Sends a GET request to the specified URL
-    and prints the value of the 'X-Requested-Id'
-    header variable from the response
-    """
-
+if __name__ == "__main__":
+    url = sys.argv[1]
     response = requests.get(url)
     x_request_id = response.headers.get('X-Request-Id')
     print(x_request_id)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        search_x_request_id(sys.argv[1])
-    else:
-        print("Usage: ./5-hbtn_header.py <https://alx-intranet.hbtn.io>")
