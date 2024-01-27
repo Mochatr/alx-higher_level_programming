41f058d3e048cab4bab97b8d5cd611280386db4c
Mochatr <mohamedchatr8@gmail.com>
2024-01-27 13:29:36 -0800

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 """
-This script takes in a URL 
-sends a request to the URL 
+This script takes in a URL
+sends a request to the URL
 and displays the body of the response.
 """
 
 import requests
 import sys
+from requests.exceptions import HTTPError
 
+if __namme__ == "__main__":
+    url = sys.argv[1]
 
-def search_url_json(url):
-    """
-    Searches content from the specified URL
-    and attempts to parse and print it as JSON
-    """
-    response = requests.get(url)
     try:
-        json_data = response.json()
-        print(json_data)
-    except ValueError:
-        print("Not a valid JSON")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        search_url_json(sys.argv[1])
-    else:
-        print("Usage: ./7-error_code.py <http://0.0.0.0:5000>")
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+    except HTTPError as err:
+        print(f"Error code: {response.status_code}")
